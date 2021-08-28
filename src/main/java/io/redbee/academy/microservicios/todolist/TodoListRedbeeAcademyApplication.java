4096725b01760be3542a4c04a2851a8678c5d308
Ignacio Valek <ignacio.valek@redb.ee>
2021-08-28 20:53:48 -0300

    Initial commit

---
+++ src/main/java/io/redbee/academy/microservicios/todolist/TodoListRedbeeAcademyApplication.java
@@ -0,0 +1,13 @@
+package io.redbee.academy.microservicios.todolist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodoListRedbeeAcademyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodoListRedbeeAcademyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/redbee/academy/microservicios/todolist/TodoListRedbeeAcademyApplicationTests.java
@@ -0,0 +1,13 @@
+package io.redbee.academy.microservicios.todolist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoListRedbeeAcademyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
