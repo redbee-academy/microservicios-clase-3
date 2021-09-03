03ccc8f1aa4526896270eee659ec86470389a04b
Ignacio Valek <ignacio.valek@redb.ee>
2021-09-03 13:53:13 -0300

    cambio de paths

--- src/main/java/io/redbee/academy/microservicios/todolist/controller/TodoListWebController.java
+++ src/main/java/io/redbee/academy/microservicios/todolist/controller/TodoListWebController.java
@@ -4,14 +4,10 @@ import io.redbee.academy.microservicios.todolist.dto.ItemAccionDto;
 import io.redbee.academy.microservicios.todolist.dto.NuevoItemDto;
 import io.redbee.academy.microservicios.todolist.model.TodoList;
 import io.redbee.academy.microservicios.todolist.service.TodoListService;
-import org.springframework.http.HttpRequest;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.util.MimeType;
 import org.springframework.web.bind.annotation.*;
-
-import javax.servlet.http.HttpServletRequest;
 
 @Controller
 public class TodoListWebController {
@@ -22,31 +18,26 @@ public class TodoListWebController {
         this.service = service;
     }
 
-    @GetMapping({"/", "/todo-list/", "/todo-list/nueva"})
+    @GetMapping(value = {"/", "/todo-list/"})
     public String nuevaTodoList(Model model) {
         TodoList todoList = service.createTodoList();
         model.addAttribute("todoList", todoList);
-        model.addAttribute("nuevoItem", new NuevoItemDto());
-        model.addAttribute("itemAccion", new ItemAccionDto());
         return "todo-list";
     }
 
-    @PostMapping("/todo-list/{listId}/items")
-    public String agregarItem(@PathVariable("listId") String listId, @ModelAttribute NuevoItemDto item, Model model) {
-        TodoList todoList = service.agregarItem(listId , item.getDetalle());
-        model.addAttribute("todoList", todoList);
-        model.addAttribute("nuevoItem", new NuevoItemDto());
-        model.addAttribute("itemAccion", new ItemAccionDto());
-        return "todo-list";
-    }
-
-    @PostMapping(value = "/todo-list/{listId}/items/{itemId}/cambiarEstado",
+    @PostMapping(value = "/todo-list/agregarItem",
             consumes = MediaType.APPLICATION_FORM_URLENCODED_VALUE)
-    public String agregarItem(@PathVariable("listId") String listId, @PathVariable("itemId") String itemId, ItemAccionDto itemAccion, Model model) {
-        TodoList todoList = service.cambiarEstadoItem(listId, itemId);
+    public String agregarItem(NuevoItemDto item, Model model) {
+        TodoList todoList = service.agregarItem(item.getListId() , item.getDetalle());
         model.addAttribute("todoList", todoList);
-        model.addAttribute("nuevoItem", new NuevoItemDto());
-        model.addAttribute("itemAccion", new ItemAccionDto());
+        return "todo-list";
+    }
+
+    @PostMapping(value = "/todo-list/cambiarEstado",
+            consumes = MediaType.APPLICATION_FORM_URLENCODED_VALUE)
+    public String agregarItem(ItemAccionDto itemAccion, Model model) {
+        TodoList todoList = service.cambiarEstadoItem(itemAccion.getListId(), itemAccion.getItemId());
+        model.addAttribute("todoList", todoList);
         return "todo-list";
     }
 }
